f4b05afdc233d66ec9a8fcf53d3d22b5bc1cce5a
Emmanuel UCSP <62064815+EmmanuelUCSP@users.noreply.github.com>
2023-03-22 17:56:40 -0500

    Add files via upload

--- 2.cpp
+++ 2.cpp
@@ -5,6 +5,12 @@
 using namespace std;
 
 int main() {
-  int n; cin>>n;
+  int n; bool cumple = false;
+  while(!cumple)
+    {
+      cin>>n;
+      if(n<=15)
+        cumple = true;
+    }
   cout << setprecision(n+1) << M_PI;
 }
